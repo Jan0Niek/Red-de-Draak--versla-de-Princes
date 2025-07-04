b19c4702d2189512d3c1234ac587deaff139cb1a
Jan_Niek <yannick.eleveld@coderclass.nl>
2025-07-04 13:39:55 +0200

    bowser draait goed rond

--- player_controller.gd
+++ player_controller.gd
@@ -9,17 +9,23 @@ var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var gravity = 998.0
 var direction = 0
+var xboostVertjump = 0
 var game_manager
 
 func _input(event):
+	xboostVertjump = 0
 	if event.is_action_pressed("jump") and is_on_floor():
 		
 		velocity.y = jump_power * jump_multiplier
+		
+	if event.is_action_pressed("vert") and is_on_floor():
+		velocity.y = -700
+		xboostVertjump = -100
 
-	if event.is_action_pressed("move_down"):
-		set_collision_mask_value(10, false)
-	else: 
-		set_collision_mask_value(10, true)
+	#if event.is_action_pressed("move_down"):
+		#set_collision_mask_value(10, false)
+	#else: 
+		#set_collision_mask_value(10, true)
 
 func _physics_process(delta: float) -> void:
 	# Voeg zwaartekracht toe als we niet op de grond zijn
@@ -30,10 +36,14 @@ func _physics_process(delta: float) -> void:
 
 	# Beweging links/rechts
 	direction = Input.get_axis("move_left", "move_right")
+	
 	if direction:
+		$Sprite2D.flip_h = -direction < 0
 		velocity.x = direction * speed * speed_multiplier
 	else:
+		#$Sprite2D.flip_h = 1
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
+	velocity.x += xboostVertjump
 
 	# Beweeg de speler
 	move_and_slide()
