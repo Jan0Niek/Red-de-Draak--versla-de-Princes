b43aeea001891c59cc5d772be81492fc03703ec1
potvisguppie <72612877+potvisguppie@users.noreply.github.com>
2025-07-04 13:41:30 +0200

    vuurbal naar rechts

--- player_controller.gd
+++ player_controller.gd
@@ -4,6 +4,9 @@ class_name PlayerController
 
 @export var speed = 10.0
 @export var jump_power = 10.0
+@export var fireball_scene: PackedScene
+
+@onready var mouth_node: Node2D = $Mouth
 
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
@@ -11,29 +14,43 @@ var gravity = 998.0
 var direction = 0
 var game_manager
 
-func _input(event):
-	if event.is_action_pressed("jump") and is_on_floor():
-		
-		velocity.y = jump_power * jump_multiplier
-
-	if event.is_action_pressed("move_down"):
-		set_collision_mask_value(10, false)
-	else: 
-		set_collision_mask_value(10, true)
-
 func _physics_process(delta: float) -> void:
-	# Voeg zwaartekracht toe als we niet op de grond zijn
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	#else:
-		#velocity.y = 0.0  # Reset springval als we op de vloer zijn
 
-	# Beweging links/rechts
 	direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed * speed_multiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
-	# Beweeg de speler
+	if Input.is_action_just_pressed("shoot"):
+		shoot_fireball()
+
 	move_and_slide()
+
+func _input(event):
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+
+	if event.is_action_pressed("move_down"):
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
+
+func shoot_fireball():
+	if fireball_scene == null:
+		return
+
+	var fireball_instance = fireball_scene.instantiate()
+
+	fireball_instance.global_position = mouth_node.global_position
+
+	var player_direction_vector = Vector2.RIGHT
+	if $Sprite2D:
+		if $Sprite2D.scale.x < 0:
+			player_direction_vector = Vector2.LEFT
+
+	fireball_instance.direction = player_direction_vector
+
+	get_tree().current_scene.add_child(fireball_instance)
