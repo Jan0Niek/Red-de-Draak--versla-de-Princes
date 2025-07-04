081e30742a9700b6dfe6f1986934d23ff8e7e3a1
Tycho-Nunnink <ruimtewezen@outlook.com>
2025-07-04 13:56:35 +0200

    mario in main en score

--- tycho's meuk/mario.gd
+++ tycho's meuk/mario.gd
@@ -1,23 +1,22 @@
 extends CharacterBody2D
-const STD_V = 100
-const GRAVITY = 200
+const JUMP_HEIGHT = 300
+const STANDARD_V = 200
+const GRAVITY = 300
 @export var score = 0
 
 func _physics_process(delta: float) -> void:
 	velocity.y += delta * GRAVITY
-	if randi() % (70 if velocity.x <= 0 else 150) == 1:
-		velocity.x = STD_V if velocity.x <= 0 else (randi() % 2) * (-STD_V)
-	
-	if is_on_floor() and randi() % 100  == 1:
-		velocity.y = -150
+	if randi() % (40 if velocity.x <= 0 else 150) == 1:
+		velocity.x = STANDARD_V if velocity.x <= 0 or position.x < -400 else (randi() % 2) * (-STANDARD_V)
+	if is_on_floor() and randi() % 40  == 1:
+		velocity.y = -JUMP_HEIGHT
 	move_and_slide()
 	
 func _init() -> void:
-	velocity.x = 100
-
-
-
-
-func _on_killplane_body_entered(body: Node2D) -> void:
-	score += 1
+	velocity.x = STANDARD_V
 	position = Vector2(0, -100)
+
+func _on_kill_plane_body_entered(body: Node2D) -> void:
+	score += 1
+	position = Vector2(-1050, -30)
+	$"../Camera2D/Control/score".text = str(score)
