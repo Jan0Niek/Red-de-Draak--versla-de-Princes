713af7c3aa771de6d403e3957759b06d6de66c2d
Jan_Niek <yannick.eleveld@coderclass.nl>
2025-07-04 15:37:09 +0200

    springen werkt i guess

--- player_controller.gd
+++ player_controller.gd
@@ -14,16 +14,30 @@ var gravity = 998.0
 var direction = 0
 var game_manager
 var flipped = false
+var lockedInJump = 0
+const lockedInTimerTime = 120
 
 func _physics_process(delta: float) -> void:
+	print(lockedInJump)
+	
+	if lockedInJump != 0:
+		lockedInJump -= 1
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	elif lockedInJump < (lockedInTimerTime-20):
+		lockedInJump = 0
 
 	direction = Input.get_axis("move_left", "move_right")
-	if direction:
+	#if not direction:	
+		#velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
+	#else:
+	if lockedInJump == 0:
 		velocity.x = direction * speed * speed_multiplier
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
+	elif lockedInJump < (0.35*lockedInTimerTime) and direction:
+		lockedInJump = 0
+		#print('dirierieu')
+			
+	
 
 	if Input.is_action_just_pressed("shoot"):
 		shoot_fireball()
@@ -40,6 +54,10 @@ func _input(event):
 	if event.is_action_pressed("move_right") and flipped == false:
 		flipped = true
 		scale.x = -1
+	if event.is_action_pressed("vert") and is_on_floor():
+		lockedInJump = lockedInTimerTime
+		velocity.y = -800
+		velocity.x = -900
 
 func shoot_fireball():
 	if fireball_scene == null:
